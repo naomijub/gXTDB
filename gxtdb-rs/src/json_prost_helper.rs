64f4b7bcac9dcaf10be688ee40ebc05fbcc43f44
Alexandra Pereira <alesilva241@gmail.com>
2023-06-16 00:40:36 -0400

    add tests for json_prost_helper
    Signed-off-by: Alexandra Pereira <alesilva241@gmail.com>

--- gxtdb-rs/src/json_prost_helper.rs
+++ gxtdb-rs/src/json_prost_helper.rs
@@ -77,3 +77,96 @@ fn parser(value: serde_json::Value) -> protobuf::Value {
         kind: Some(protobuf::value::Kind::NullValue(0)),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{api::google::protobuf::{self}, json_prost_helper::parser};
+    use serde_json::json;
+
+    #[test]
+    fn parser_bool() {
+        let v = json!(true);
+        assert_eq!(
+            (parser(v)),
+            protobuf::Value {
+                kind: Some(protobuf::value::Kind::BoolValue(true)),
+            }
+        );
+    }
+
+    #[test]
+    fn parser_f64() {
+        let v = json!(3.14);
+        assert_eq!(
+            (parser(v)),
+            protobuf::Value {
+                kind: Some(protobuf::value::Kind::NumberValue(3.14)),
+            }
+        );
+    }
+    #[test]
+    fn parser_i64() {
+        let v = json!(3.14);
+        assert_eq!(
+            (parser(v)),
+            protobuf::Value {
+                kind: Some(protobuf::value::Kind::NumberValue(3.14)),
+            }
+        );
+    }
+
+    #[test]
+    fn parser_u64() {
+        let v = json!(64.0);
+        assert_eq!(
+            (parser(v)),
+            protobuf::Value {
+                kind: Some(protobuf::value::Kind::NumberValue(64.0)),
+            }
+        );
+    }
+
+    #[test]
+    fn parser_null() {
+        let v = json!(null);
+        assert_eq!(
+            (parser(v)),
+            protobuf::Value {
+                kind: Some(protobuf::value::Kind::NullValue(0)),
+            }
+        );
+    }
+
+    #[test]
+    fn parser_string() {
+        let v = json!("gxtdb-rs".to_string());
+        assert_eq!(
+            parser(v),
+            protobuf::Value {
+                kind: Some(protobuf::value::Kind::StringValue("gxtdb-rs".to_string())),
+            }
+        );
+    }
+
+    // #[test]
+    // fn parser_array(){
+    //     let v = json!([1, 2, 3]);
+    //     assert_eq!(
+    //         parser(v),
+    //         protobuf::Value {
+    //             kind: Some(protobuf::value::Kind::ListValue(values : [1, 2, 3])),
+    //         }
+    //     );
+    // }
+
+    // #[test]
+    // fn parser_map(){
+    //     let v = json!("a": 1, "b": 2);
+    //     assert_eq!(
+    //         parser(v),
+    //         protobuf::Value {
+    //             kind: Some(protobuf::value::Kind::StructValue("a": 1, "b": 2))
+    //         }
+    //     )
+    // }
+}
