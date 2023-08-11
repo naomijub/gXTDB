4b5ad41bd5cc2af06059a0021cc375e89905038c
Alexandra Pereira <alesilva241@gmail.com>
2023-08-10 23:22:08 -0400

    revert ->db-basis refactor
    Signed-off-by: Alexandra Pereira <alesilva241@gmail.com>

--- src/gxtdb/adapters/db.clj
+++ src/gxtdb/adapters/db.clj
@@ -1,5 +1,21 @@
 (ns gxtdb.adapters.db
-  (:require [gxtdb.utils :as utils]))
+  (:require [gxtdb.adapters.tx-time :refer [->clj-time]]
+            [gxtdb.utils :refer [not-nil?]]
+            [gxtdb.utils :as utils]
+            [xtdb.api :as xt]))
+
+(defn ->db-basis [params]
+  (let [valid-time (some-> params :valid-time ->clj-time)
+        tx-time (some-> params :tx-time ->clj-time)
+        tx-id (some-> params :tx-id :value :some)]
+    (case [(not-nil? valid-time) (not-nil? tx-time) (not-nil? tx-id)]
+      [true true true] {::xt/valid-time valid-time}
+      [true false false] {::xt/valid-time valid-time}
+      [true true false] {::xt/valid-time valid-time ::xt/tx-time tx-time}
+      [true false true] {::xt/valid-time valid-time ::xt/tx tx-id}
+      [false true false] {::xt/tx-time tx-time}
+      [false false true] {::xt/tx tx-id}
+      nil)))
 
 (defn db-basis->proto [db-basis-response]
   {:xt-id (-> db-basis-response :xt/id str)
